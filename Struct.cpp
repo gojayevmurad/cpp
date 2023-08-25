8404be814710b9fa84e1bc885d0798c7c438aad2
Murad Gojayev <muradqocayevi20@gmail.com>
2023-08-26 00:34:32 +0400

    sturct completed

--- Struct.cpp
+++ Struct.cpp
@@ -23,9 +23,9 @@ void CreateHuman(Human* human, int age, char gender, const string name) {
 }
 
 struct Player {
-	double score;
-	char* nickname;
-	bool isStar;
+	double score; // 8
+	char* nickname; // 8
+	bool isStar; // 1
 };
 
 void ShowPlayer(const Player& player) {
@@ -91,8 +91,123 @@ void setNewObj(Player& player) {
 
 }
 
+struct Figure{
+	double width;
+	double height;
+	char* name;
+};
+
+
+void ShowFigure(Figure& figure) {
+	cout << "Height : " << figure.height << endl;
+	cout << "Width : " << figure.width << endl;
+	cout << "Name : " << figure.name << endl;
+}
+
+struct Teacher {
+	char* name;
+	char* speciality;
+};
+
+struct Student {
+	Teacher myTeacher;
+	char* name;
+	int age;
+};
+
+void ShowTeacher(const Teacher& teacher) {
+	cout << "Teacher Info" << endl;
+	cout << "Name : " << teacher.name << endl;
+	cout << "Speciality : " << teacher.speciality << endl;
+}
+
+void ShowStudent(const Student& student) {
+	ShowTeacher(student.myTeacher);
+
+	cout << "Student Info" << endl;
+	cout << "Name : " << student.name << endl;
+	cout << "Age : " << student.age << endl;
+
+}
+
+const Teacher* GetNewTeacher() {
+	Teacher teacher;
+
+	cout << "Enter name : " << endl;
+	char* name = new char[10] {};
+	cin.getline(name, 10);
+
+	cout << "Enter Speciality : " << endl;
+	char* speciality = new char[10] {};
+	cin.getline(speciality, 10);
+
+	teacher.name = name;
+	teacher.speciality = speciality;
+
+	return &teacher;
+}
+
+const Student* GetNewStudent() {
+	Student student;
+
+	cout << "Enter name : " << endl;
+	char* name = new char[10] {};
+	cin.getline(name, 10);
+
+	cout << "Enter age : " << endl;
+	int age;
+	cin >> age;
+
+	cin.ignore();
+	cin.clear();
+
+	const Teacher* teacher = GetNewTeacher();
+
+	student.age = age;
+	student.name = name;
+	student.myTeacher = *teacher;
+
+	return &student;
+}
+
 void main() {
-	Player player;
-	setNewObj(player);
-	ShowPlayer(player);
+	//Player player;
+	//setNewObj(player);
+	//ShowPlayer(player);
+
+	//cout << sizeof(Player) << endl; // 24 byte
+
+	/*
+		Figure f1{ 100, 500, new char[30] {"Rectangle"} };
+		Figure f2{ 100, 500, new char[30] {"Rectangle"} };
+		Figure figures[2]{ f1,f2 };
+
+		for (size_t i = 0; i < 2; i++){
+			ShowFigure(figures[i]);
+		}
+	*/
+
+	/*
+		Figure* f1 = new Figure{ 100, 500, new char[30] {"Rectangle"} };
+		Figure* f2 = new Figure{ 100, 500, new char[30] {"Rectangle"} };
+
+		Figure** figures = new Figure * [2] {f1, f2};
+
+		for (size_t i = 0; i < 2; i++){
+			ShowFigure(*figures[i]);
+		}
+	*/
+
+	/*
+		Teacher t{ new char[10] {"Leyla"}, new char[30] {"UX Designer"} };
+		Student s{ t, new char[10] {"Arif"}, 35 };
+
+		ShowStudent(s);
+	*/
+
+	// get new student
+	const Student* newStudent = GetNewStudent();
+
+	ShowStudent(*newStudent);
+
 }
